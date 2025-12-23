804a427df5640bde73dd795e28a77f5f4dc54c1c
Mouiz Majid <Mouiz Majid>
2025-12-23 01:42:21 -0500

    more react code idk

--- React/reactTutorial/src/App.jsx
+++ React/reactTutorial/src/App.jsx
@@ -1,20 +1,8 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { Fragment } from 'react'
-import NavBar from './components/Navbar.jsx'
-import Place from './components/Place.jsx'
-import data from './data.jsx'
+
 import Landing from './components/Landing.jsx'
 import Header from './components/Header.jsx'
 
-
-// const root = createRoot(document.getElementById('root'))
-
 function App() {
-
-  // const places = data.map((item) => {
-  //   return <Place key={item.id} src={item.src} name={item.title} country={item.country}  date={item.date} info={item.info} />
-  // })
 
 
   return (<div className="main">
--- React/reactTutorial/src/components/Landing.jsx
+++ React/reactTutorial/src/components/Landing.jsx
@@ -2,35 +2,46 @@ import React from 'react'
 
 function Landing() {
 
-    const [ingredients, setIngredients] = React.useState(["1 cup a", "2 eggs", "1/2 cup of sugar"]);
+    const [ingredients, setIngredients] = React.useState([]);
+    const [recipeShown, setRecipeShown] = React.useState(false);
 
     const ingredientList = ingredients.map((item, index) => <li key={index} >{item}</li>)
     
     
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        const formdata = new FormData(event.currentTarget);
+    function addIngredient(formdata) {
         const newIngredient = formdata.get("ingredient");
         setIngredients(prevState => [...prevState, newIngredient]);
-        
     }
     return(
         <div className="landing">
-            <form className="searchBar" onSubmit={handleSubmit} >
-                <input type="text" placeholder="e.g oregano"  name="ingredient" ></input>
+
+            <form className="searchBar" action={addIngredient}  >
+                <input type="text" placeholder="e.g oregano"  name="ingredient" required></input>
                 <input type="submit" value="+ Add ingredient"></input>
             </form>
-            <ul> Ingredients on hand: 
+
+            {ingredients.length > 0 ? <ul> Ingredients on hand: 
                 {ingredientList}
-            </ul>
+            </ul> : null}
+            
+            {ingredients.length > 3 ? 
             <div className="getRecipe">
                 <div className="text-content">
                     <h2>Ready for a recipe?</h2>
                     <p>Generate a recipe from your list of ingredients</p>
                 </div>
-                <button>Get Recipe</button>
-            </div>
+                <button onClick={() => setRecipeShown(prev => !prev)}>Get Recipe</button>
+            </div> : null }
+             
+             
+
+            {recipeShown ? <div><h2>Suggested Recipe</h2> <p>hello</p></div> : null}
+            
+
+
+
+
         </div>
     )
 }
